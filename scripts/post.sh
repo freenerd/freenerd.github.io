22b19c3d5c88e726c215f2ae0d720df5e8e95f7d
Freenerd <nospam@freenerd.de>
2014-01-11 17:42:26 +0100

    new post script

---
+++ scripts/post.sh
@@ -0,0 +1,18 @@
+# Created a new post with title and current date
+#
+# Usage: ./scripts/post.sh My new post
+
+date=$(date +%F)
+
+filename="$date-$( echo $1 | tr "A-Z" "a-z" | tr " " "-" ).md"
+
+post=$(cat <<POST
+---
+layout:   post
+title:    $1
+---
+POST)
+
+echo "$post" > "./_posts/$filename"
+
+mvim "./_posts/$filename"
